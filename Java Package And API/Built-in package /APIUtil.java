7691c3510be9fbcd7150815c637336113ea7fcb9
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-15 21:39:08 +0530

    Create APIUtil.java

---
+++ Java Package And API/Built-in package /APIUtil.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+public class APIUtil {
+    public static void main(String[] args){
+        List <String>names=new ArrayList<>();
+        names.add("Alice");
+        names.add("Ahmed");
+        names.add("Saloni");
+        names.add("Bob");
+        names.add("Akiop");
+        System.out.println("names added are: "+ names);
+    }
+}
