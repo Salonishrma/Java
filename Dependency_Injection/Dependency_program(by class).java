9ce50ba7cd6494990da523b6cbd19e69dcf386bb
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-15 17:07:27 +0530

    Create Dependency_program(by class).java

---
+++ Dependency_Injection/Dependency_program(by class).java
@@ -0,0 +1,14 @@
+//service interface
+interface MessageService{
+    String getMessage();
+}
+//implementation of Message Service
+class EmailService implements MessageService{
+    @Override
+    public String getMessage(){
+        return "Email Message".
+    }
+}
+public class Dependency_program{
+
+}
