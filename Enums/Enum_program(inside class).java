e0fe3de06489240a1ee82a56140a6ca517aba57d
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-15 17:34:50 +0530

    Create Enum_program(inside class).java

---
+++ Enums/Enum_program(inside class).java
@@ -0,0 +1,13 @@
+//ENUM creation
+public class Enum_program{
+enum Level{
+    LOW,
+    MEDIUM,
+    HIGH
+}
+    public static void main(String[]args){
+        Level myVar = Level.MEDIUM;
+        Level myVar1=Level.HIGH;
+        System.out.println(myVar1);
+    }
+}
