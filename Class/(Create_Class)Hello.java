994568e0172028790c8b4e19473c011978eaf2c1
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-16 12:25:16 +0530

    Create (Create_Class)Hello.java

---
+++ Class/(Create_Class)Hello.java
@@ -0,0 +1,12 @@
+public class Hello {public class Hello {
+int x=5;
+String name="saloni";
+public static void main(String[] args) {
+Hello h1=new Hello(); //creating object of class Hello
+System.out.println(h1.x); //accesssing the object
+System.out.println(h1.name);
+Hello h2=new Hello();//creating one more object
+System.out.println(h2.x); //accesssing the object
+System.out.println(h2.name);
+}
+}
