aa5dcf48da24f463f479b43495c526e40c937333
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-16 12:29:36 +0530

    Create Modyying_attribute.java

---
+++ Class/Modyying_attribute.java
@@ -0,0 +1,18 @@
+public class Hello {
+int x=5;
+Hello h1=new Hello(); //creating object of class Hello
+System.out.println(h1.x); //accesssing the object
+System.out.println(h1.name);
+String name="saloni";
+public static void main(String[] args) {
+Hello h1=new Hello();
+System.out.println(h1.x);
+System.out.println(h1.name);
+new
+}
+Hello h2=new Hello();
+System.out.println(h2.x);
+System.out.println(h2.name);
+h2.name="Radhe RAdhe"; //here we assign new value
+System.out.println(h2.name); // output here will show
+}
