e076894f5420fe3fe30e8b58606e7cb150f968ee
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-16 12:34:29 +0530

    Create Return_Method.java

---
+++ Method/Return_Method.java
@@ -0,0 +1,8 @@
+public class Hello {
+    static int add(int a,int b){ 
+        return a+b;
+    }
+    public static void main(String[] args) {
+       int sum = add(5,6);
+       System.out.println("sum is equal to = "+sum);
+   
