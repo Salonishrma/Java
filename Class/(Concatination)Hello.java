fdda0bcd5de0058c0b963e65638f8dca80efa41c
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-16 12:30:27 +0530

    Create (Concatination)Hello.java

---
+++ Class/(Concatination)Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+int x=5;
+String fname="saloni";
+String lname ="sharma";
+public static void main(String[] args) {
+"+myobj.lname);
+}
+Hello myobj=new Hello();
+System.out.println("NAME" + " : "+myobj.fname +"
+}
