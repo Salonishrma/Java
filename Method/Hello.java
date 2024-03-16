1b938e42e469c3e4913dc54d5b7a7e9160cf7751
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-16 12:32:07 +0530

    Create Hello.java

---
+++ Method/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+ static void myMethod(){ //creating method
+System.out.println("hey my world");
+ }
+public static void main(String[] args) {
+  myMethod(); //calling my method
+ }
+ }
