876a52af4e63bc687033964073186bfbcaff4a98
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-16 12:27:48 +0530

    Delete Clas( multiple classes)Hello_2.java

--- Clas( multiple classes)Hello_2.java
+++
@@ -1,7 +0,0 @@
-public class Hello_2{
-public static void main(String[] args) {
-  Hello h1=new Hello(); //creating object of class Hello
-System.out.println(h1.x); //accesssing the object
-System.out.println(h1.name);
-   }
- }
