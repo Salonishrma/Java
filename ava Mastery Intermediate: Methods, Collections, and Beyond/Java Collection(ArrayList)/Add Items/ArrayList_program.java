444b7e871b6e111b21cc7755aec486a049e7d82c
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-18 15:01:53 +0530

    Create ArrayList_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Java Collection(ArrayList)/Add Items/ArrayList_program.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList; //import the arraylist library
+public class ArrayList_program{
+    public static  void main(String[] args) {
+        //ArrayList declaration
+        ArrayList<String>fruits = new ArrayList<String>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Kiwi");
+
+        System.out.println(fruits);
+    }
+}
