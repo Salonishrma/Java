b66849f8cccf6ebf76c30b311fdf02f2d6f9a71c
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-18 14:27:50 +0530

    Create (Variable Scope)variable_scope.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Method/(Variable Scope)variable_scope.java
@@ -0,0 +1,9 @@
+public class Variable_Scope {
+    public static  void main(String[] args) {
+        //declaration or creation of a variable
+        int x=20;
+        //aceessing this variable
+        System.out.println("Variable is accessible " + x);
+    }
+    //we cannot acesss x here 
+}
