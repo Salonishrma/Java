a62c05274b664bbb23fdfd007178d7673dc2de51
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-18 13:47:52 +0530

    Create (Method Parameter)Method_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/(Method Parameter)Method_program.java
@@ -0,0 +1,13 @@
+public class Method_program {
+    
+
+    static void greetings(String fname){
+        System.out.println("Welcome to java development");
+        System.out.println("Hello "+fname);
+    }
+
+    public static void main(String[] args){
+        greetings("Saloni");
+        greetings("Meryy");
+    }
+}
