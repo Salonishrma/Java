b36a6aa9637dbb61899fc7ed0b6ba481fc5c4651
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-18 13:59:42 +0530

    Create (for int)Method_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Applying Return/(for int)Method_program.java
@@ -0,0 +1,11 @@
+public class Method_program {
+
+    static int myAdd(int a,int b){
+        //using a return keyword
+        return a + b;
+    }
+    public static void main(String[] args){
+        int c=myAdd(4,6);
+      System.out.println(c);
+    }
+}
