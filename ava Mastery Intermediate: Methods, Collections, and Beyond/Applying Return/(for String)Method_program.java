4728b86351f6b04004a8db02338469f348c650b6
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-18 14:01:33 +0530

    Create (for String)Method_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Applying Return/(for String)Method_program.java
@@ -0,0 +1,11 @@
+public class Method_program {
+
+    static String myName(String fname,String lname){
+        //using a return keyword
+        return fname+ lname;
+    }
+    public static void main(String[] args){
+        String c=myName("gita ","sita");
+      System.out.println(c);
+    }
+}
