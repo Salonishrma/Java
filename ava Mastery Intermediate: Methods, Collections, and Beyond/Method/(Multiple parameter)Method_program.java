5d93fa1fbdc75206bf3931df07f24d632d543161
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-18 14:05:14 +0530

    Create (Multiple parameter)Method_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Method/(Multiple parameter)Method_program.java
@@ -0,0 +1,10 @@
+public class Method_program {  
+  static void greetings(String firstname,int age){
+        System.out.println("Welcome to java development");
+        System.out.println("Hello "+firstname + " Your age is "+age);
+    }
+    public static void main(String[] args){
+        greetings("Saloni",9);
+        greetings("Meryy",8);
+    }
+}
