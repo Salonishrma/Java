0ba9b866960af29f3d3d2d8dc3c359465eac7735
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-19 14:43:00 +0530

    Create  HashMap_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/HashMap/Create HashMap/HashMap_program.java
@@ -0,0 +1,5 @@
+import java.util.HashMap;
+public class HashMap_program {
+    //create HashMap object called capitalCities
+    HashMap<String,String>capitalCities= new HashMap<String,String>();
+}
