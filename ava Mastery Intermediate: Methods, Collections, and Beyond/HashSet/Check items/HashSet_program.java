a67d35f6010e8e3ef25408118cc2109796cdeef5
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-19 16:29:57 +0530

    Create HashSet_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/HashSet/Check items/HashSet_program.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+public class HashSet_program {
+    public static  void main(String [] args){
+        //creating hashSet
+     HashSet<String>sports=new HashSet<String>();
+     sports.add("Football");
+     sports.add("Tennis");
+     sports.add("Cricket");
+     sports.add("Cricket");
+     sports.add("Badminton");
+     System.out.println(sports.contains("Tennis"));
+     System.out.println(sports.contains("Sleep"));
+    }
