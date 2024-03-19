7ba78044f5b103ecd12c5227811b860f97439cd3
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-19 14:49:40 +0530

    Create HashMap_program.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/HashMap/Access an Item/HashMap_program.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+public class HashMap_program {
+    public static void main(String[] args) {
+        //create HashMap object called capitalCities
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        //adding keys and values (Countries,City)
+        capitalCities.put("England","London");
+        capitalCities.put("Germany","Berlin");
+        capitalCities.put("India","Delhi");
+        capitalCities.put("USA","NY");
+        System.out.println(capitalCities.get("England"));
+    }
+}
