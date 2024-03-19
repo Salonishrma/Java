e59c27bb5767cb29deef244f705378cf6845c84e
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-19 16:24:52 +0530

    Create HashSet_program .java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/HashSet/Create HashSet/HashSet_program .java
@@ -0,0 +1,8 @@
+import java.util.HashSet;
+public class HashSet_program {
+    public static  void main(String [] args){
+        //creating hashSet
+     HashSet<String>Sports=new HashSet<String>();
+
+    }
+}
