07a6d134efce293278e8e505b81bc67989374f3e
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-20 17:02:40 +0530

    Create Error_Handling .java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Java Error Handling and Exceptions/Try and catch Block /Error_Handling .java
@@ -0,0 +1,13 @@
+public class Error_Handling {
+    //try - catch block of coding
+    public static void main(String[] args){
+        //code will be written here
+        try{
+            int[] myNum={1,2,3,4,5};
+            System.out.println(myNum[10]); //error wrong index number give
+        }
+        catch (Exception e){
+            System.out.println("something went wrong");
+        }
+    }
+}
