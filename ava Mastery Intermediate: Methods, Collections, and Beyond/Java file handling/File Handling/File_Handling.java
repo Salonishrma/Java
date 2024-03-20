ed21f79feb1d7ae31101d660d620c4a1f6a2cb63
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-20 15:16:51 +0530

    Create File_Handling.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Java file handling/File Handling/File_Handling.java
@@ -0,0 +1,7 @@
+import java.io.File; //import the File class library
+public class File_Handling {
+    public static void main(String[] args){
+        //New file object creation
+        File myObj =new File("myfile.txt");
+    }
+}
