e1fe90ac66e65a17f07b059735a7eb5b2c26e6cd
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-21 16:06:48 +0530

    Create AuthentiationSystem .java

---
+++ Java Project :User Authentication System /User Login Method/AuthentiationSystem .java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+class User{
+    private String username;
+    private  String password;
+
+    public User(String username,String password){
+        this.username=username;
+        this.password=password;
+    }
+
+    public String getUsername(){
+        return  username;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
+public class AuthentiationSystem {
+    private LinkedList<User>userList;
+    public AuthentiationSystem(){
+        userList =new LinkedList<>();
+    }
+
+    public void register(String username, String password){
+        User newUser = new User(username,password);
+        userList.add(newUser);
+        System.out.println("User Registered Successfully.");
+    }
+    public boolean login(String username, String password){
+        for(User user:userList){
+            if(user.getUsername().equals(username) && user.getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        AuthentiationSystem authSystem =new AuthentiationSystem();
+        Scanner scanner=new Scanner(System.in);
+        boolean loggedIn= false;
+        while(!loggedIn){
+            System.out.println("Choose an Option");
+            System.out.println("1.Register");
+            System.out.println("2.Login");
+            System.out.println("3.Exit");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+        }
+    }
+
+}
