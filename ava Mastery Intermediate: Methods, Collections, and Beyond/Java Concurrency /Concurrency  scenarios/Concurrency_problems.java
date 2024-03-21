d51534d61c9a18b9cbfd778aa9cfcaf0abc23f0a
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-21 15:03:19 +0530

    Create Concurrency_problems.java

---
+++ ava Mastery Intermediate: Methods, Collections, and Beyond/Java Concurrency /Concurrency  scenarios/Concurrency_problems.java
@@ -0,0 +1,13 @@
+public class Concurrency_problems extends Thread{
+    public static  int amount=0;
+    public static void main(String[] args) {
+        Concurrency_problems thread=new Concurrency_problems();
+        thread.start();
+        System.out.println(amount);
+        amount++;
+        System.out.println(amount);
+    }
+    public void run(){
+        amount++;
+    }
+}
